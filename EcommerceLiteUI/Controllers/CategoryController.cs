cfdd9f98dc642ff26f47dd11f5357503eaa3e230
103SABAH_BEYZANUR <103SABAH_BEYZANUR@DESKTOP-TUMHS1A>
2022-01-21 12:14:54 +0300

    CategoryController'a ait sayfalar eklendi.

--- EcommerceLiteUI/Controllers/CategoryController.cs
+++ EcommerceLiteUI/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EcommerceLiteBLL.Repository;
+using EcommerceLiteEntity.Models;
 
 namespace EcommerceLiteUI.Controllers
 {
@@ -16,6 +17,68 @@ namespace EcommerceLiteUI.Controllers
             var allCategories = myCategoryRepo.Queryable().Where(x=> x.BaseCategoryId==null).ToList();
             ViewBag.CategoryCount = allCategories.Count;
             return View(allCategories);
+        }
+        public ActionResult Create(int? id)
+        {
+            if (id!=null)
+            {
+                Category model = new Category() 
+                { 
+                    Id=id.Value
+                };
+                return View(model);
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    ModelState.AddModelError("", "Geçersiz veri girişi! Lütfen verileri düzgün giriniz!");
+                    return View(model);
+                }
+                Category newCategory = new Category() 
+                { 
+                    CategoryName=model.CategoryName,
+                    CategoryDescription=model.CategoryDescription,
+                    RegisterDate=DateTime.Now,
+                    BaseCategoryId=null
+                };
+                if (model.Id>0)
+                {
+                    newCategory.BaseCategoryId = model.Id;
+                }
+                int insertResult = myCategoryRepo.Insert(newCategory);
+                if (insertResult>0 && model.Id==0)
+                {
+                    return RedirectToAction("CategoryList", "Category");
+                }
+                else if (insertResult > 0 && model.Id >0)
+                {
+                    return RedirectToAction("SubCategoryList", "Category");
+                }
+                else
+                {
+                    throw new Exception("Kategori eklenemedi!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Beklenmedik bir hata oluştu! Tekrar deneyiniz! HATA:" + ex.Message);
+                return View(model);
+            }
+        }
+        public ActionResult SubCategoryList(int id)
+        {
+            var subCategories = myCategoryRepo.Queryable().Where(x => x.BaseCategoryId != null && x.BaseCategoryId==id).ToList();
+            ViewBag.CategoryId = id;
+            ViewBag.CategoryName = myCategoryRepo.GetById(id).CategoryName;
+            ViewBag.SubCategoryCount = subCategories.Count;
+            return View(subCategories);
         }
     }
 }

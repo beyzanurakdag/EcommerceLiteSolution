aef8eeb33a7d8c00351309e1bf7df2902f77c139
103SABAH_BEYZANUR <103SABAH_BEYZANUR@DESKTOP-TUMHS1A>
2022-01-21 15:02:03 +0300

    ProductController eklendi.

---
+++ EcommerceLiteUI/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EcommerceLiteBLL.Repository;
+using EcommerceLiteEntity.Models;
+
+namespace EcommerceLiteUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //Global Alan
+        ProductRepo myProductRepo = new ProductRepo();
+        CategoryRepo myCategoryRepo = new CategoryRepo();
+        public ActionResult ProductList()
+        {
+            var allProductList = myProductRepo.GetAll();
+            return View(allProductList);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            List<SelectListItem> allCategories = new List<SelectListItem>();
+            myCategoryRepo.GetAll().ToList().ForEach(x => allCategories.Add(new SelectListItem()
+            {
+                Text = x.CategoryName,
+                Value = x.Id.ToString()
+            }));
+            ViewBag.CategoryList = allCategories;
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    ModelState.AddModelError("", "Veri girişleri düzgün olmalıdır!");
+                    return View(model);
+                }
+                int insertResult=myProductRepo.Insert(model);
+                if (insertResult>0)
+                {
+                    return RedirectToAction("ProductList", "Product");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Ürün ekleme işleminde bir hata oluştu! Tekrar deneyiniz!");
+                    return View(model);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Beklenmedik bir hata oluştu!");
+                return View(model);
+            }
+        }
+    }
+}
